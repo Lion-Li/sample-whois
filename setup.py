b2651c0c1a34891a9917a30b213dcfc104557860
lixiang <121365554@qq.com>
2019-11-19 09:17:00 +0800

    init

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+
+setup(
+    name="sample-whois",
+    version="0.2",
+    description="域名注册信息查询工具",
+    author="lixiang",
+    author_email="121365554@qq.com",
+    url="https://github.com/Lion-Li",
+    license="MIT",
+    packages=["whois"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries",
+        # "License :: OSI Approved :: MIT Licence",
+        "Programming Language :: Python :: 3.6",
+    ]
+)
